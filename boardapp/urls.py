25e5b399b3be132c63c6f18e4de2eabb3b65140c
Yuya Ohbayashi <yuya9445@gmail.com>
2021-06-12 15:37:40 +0900

    git rm -r --cached . を実行

--- boardapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import BoardList, BoardDetail
-
-urlpatterns = [
-    path('list/', BoardList.as_view()),
-    path('detail/<int:pk>', BoardDetail.as_view()),
-]
